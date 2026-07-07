c5ad665590b4a956592cc3e374f7b04c321eadec
Aniket Verma <aniketverma000081@gmail.com>
2026-07-07 16:54:07 +0530

    aded

--- src/components/layout/TopNav.jsx
+++ src/components/layout/TopNav.jsx
@@ -1,8 +1,8 @@
 const TopNav = () => {
   return (
     <div className="fixed top-0 left-0 w-full z-[60] h-5 bg-white ">
-      <div className="container-custom h-full flex items-center  md:justify-end-safe  text-[#111111]">
-        <span className="text-xl font-medium pt-4">
+      <div className="container-custom h-full flex items-center justify-end-safe  text-[#111111]">
+        <span className="text-[14px] xs:text-xs sm:text-sm md:text-base lg:text-lg font-medium text-center">
           <span className="text-[#C9A227]">REG ID : </span>
            UDYAM-UP-75-0191510
         </span>
